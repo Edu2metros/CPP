c0d82e8286bb1db9dba6497cbdc45bc664347d71
edu <eddos-sa@eddos-sa.eddos-sa>
2024-05-22 15:41:10 -0300

    sed done

---
+++ sed/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstring>
+
+std::ofstream filename_out(char *filename)
+{
+	const char *extesion = ".replace";
+	size_t len1 = std::strlen(filename);
+	size_t len2 = std::strlen(extesion);
+	char *result = new char[len1 + len2 + 1];
+	std::strcpy(result, filename);
+	std::strcat(result, extesion);
+	std::ofstream outfile(result);
+	delete[] result;
+	return (outfile);
+}
+
+void write_outfile(std::ifstream &infile, std::ofstream &outfile, char *str_find, char *str_replace)
+{
+    std::string line;
+    while(std::getline(infile, line)){
+        std::string new_line;
+        for(size_t i = 0; i < line.size();){
+            if(line.substr(i, std::strlen(str_find)) == str_find){
+                new_line += str_replace;
+                i += std::strlen(str_find);
+            } else {
+                new_line += line[i];
+                i++;
+            }
+        }
+        outfile << new_line << std::endl;
+    }
+}
+
+int main(int argc, char **argv)
+{
+	if(argc != 4){
+		std::cerr << "Error! Invalid usage of the program. \
+The program can only take 3 parameters. <filename> string1 string2." << std::endl;
+		return(1);
+	}
+	std::ifstream infile(argv[1]);
+	if(!infile.is_open()){
+		std::cerr << "Error reading the input file." << std::endl;
+		return(1);
+	}
+	std::ofstream outfile = filename_out(argv[1]);
+	if(!outfile.is_open()){
+		std::cerr << "Error creating the output file." << std::endl;
+		return(1);
+	}
+	write_outfile(infile, outfile, argv[2], argv[3]);
+	return(0);
+}
